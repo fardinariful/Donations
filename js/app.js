f45e861df796811f719c08bca6edc35085a6f277
fardinariful <fardinewucse2020@gmail.com>
2024-09-25 11:16:42 +0600

    something added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const allbtn=document.getElementsByClassName("all-btn");
+for(const btn of allbtn)
+{
+    btn.addEventListener("click",function(e){
+        console.log("fardin")
+    })
+    //console.log(btn);
+}
